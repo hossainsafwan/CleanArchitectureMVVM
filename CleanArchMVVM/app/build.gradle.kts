1a52c900eb7d674a6795fa280fadf5c6a8636b27
hossainsafwan <safwanhossain@live.com>
2023-12-16 19:49:02 -0500

    label build.gradle.kts file

--- CleanArchMVVM/app/build.gradle.kts
+++ CleanArchMVVM/app/build.gradle.kts
@@ -40,13 +40,13 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    // Extension to Android Framework
+    implementation("androidx.core:core-ktx:1.12.0")
+
+    // UI
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     //Retrofit and GSON
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
@@ -63,5 +63,8 @@ dependencies {
     //Swipe to refresh
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
 
-
+    //Testing
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
