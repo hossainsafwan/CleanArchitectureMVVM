0449c7fd587360ac27932930c735a0d9b3b2e1c5
hossainsafwan <safwanhossain@live.com>
2023-12-26 04:54:34 -0500

    use transform inside use case

--- CleanArchMVVM/app/src/main/java/com/example/cleanarchitecture/domain/usecases/GetCountryUseCaseImpl.kt
+++ CleanArchMVVM/app/src/main/java/com/example/cleanarchitecture/domain/usecases/GetCountryUseCaseImpl.kt
@@ -4,33 +4,30 @@ import com.example.cleanarchitecture.domain.models.Country
 import com.example.cleanarchitecture.data.repositories.AllCountriesRepository
 import com.example.cleanarchitecture.util.Resource
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.transform
 
 class GetCountryUseCaseImpl(
     private val countryRepository: AllCountriesRepository
 ) : GetCountryUseCase {
-    override suspend fun invoke(): Flow<Resource<List<Country>>> {
-        lateinit var output: Flow<Resource<List<Country>>>
-        countryRepository.getAllCountries().collectLatest { resource ->
+    override suspend fun invoke(): Flow<Resource<List<Country>>> =
+        countryRepository.getAllCountries().transform { resource ->
             when (resource) {
                 is Resource.Loading -> {
-                    output = flowOf(Resource.Loading())
+                    emit(Resource.Loading())
                 }
 
                 is Resource.Error -> {
-                    output = flowOf(Resource.Error(message = resource.message))
+                    emit(Resource.Error(message = resource.message))
                 }
 
                 is Resource.Success -> {
                     val mapper = resource.data?.map {
-                        Country(it.name,it.code,it.image)
+                        Country(it.name, it.code, it.image)
                     }
-                    output = flowOf(Resource.Success(data = mapper))
+                    emit(Resource.Success(data = mapper))
                 }
             }
 
         }
-        return output
-    }
+
 }
