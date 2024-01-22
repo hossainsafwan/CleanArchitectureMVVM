dbdc0e8123d5cbf0d8b87b093c37ec01a245bcc1
hossainsafwan <safwanhossain@live.com>
2024-01-22 16:30:36 -0500

    initialize viewmodel lazily by kotlin property delegate

--- CleanArchMVVM/app/src/main/java/com/example/cleanarchitecture/ui/MainActivity.kt
+++ CleanArchMVVM/app/src/main/java/com/example/cleanarchitecture/ui/MainActivity.kt
@@ -2,10 +2,10 @@ package com.example.cleanarchitecture.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.example.cleanarchitecture.data.network.RetrofitInstance
 import com.example.cleanarchitecture.data.repositories.AllCountriesRepositoryImpl
 import com.example.cleanarchitecture.databinding.ActivityMainBinding
@@ -21,20 +21,21 @@ class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding
         get() = _binding!!
 
+    private val viewModel: CountryViewModel by viewModels {  CountryViewModelFactory(
+        GetCountryUseCaseImpl(
+            AllCountriesRepositoryImpl(RetrofitInstance.countryAPI)
+        )
+    ) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val viewModelFactory = CountryViewModelFactory(
-            GetCountryUseCaseImpl(
-                AllCountriesRepositoryImpl(RetrofitInstance.countryAPI)
-            )
-        )
-        val viewModel = ViewModelProvider(this, viewModelFactory)[CountryViewModel::class.java]
+
         val adapter = CountryListAdapter()
         binding.countryList.adapter = adapter
         viewModel.countryModel?.observe(this, Observer {
-            showSearchedList(adapter,viewModel)
+            showSearchedList(adapter)
             binding.progressBar.isVisible = it.isLoading
             it.errorMessage?.let { errorMessage ->
                 Snackbar.make(
@@ -51,18 +52,18 @@ class MainActivity : AppCompatActivity() {
 
         binding.searchBar.addTextChangedListener { editable ->
             viewModel.searchQuery(editable.toString())
-            showSearchedList(adapter,viewModel)
+            showSearchedList(adapter)
         }
 
         binding.swipeToRefresh.setOnRefreshListener {
             binding.swipeToRefresh.isRefreshing = false
             viewModel.getCountries()
-            showSearchedList(adapter,viewModel)
+            showSearchedList(adapter)
         }
 
     }
 
-    private fun showSearchedList(adapter: CountryListAdapter, viewModel: CountryViewModel) {
+    private fun showSearchedList(adapter: CountryListAdapter) {
         val searchList = if (viewModel.searchQuery.isEmpty()) {
             viewModel.countryModel?.value?.countryList ?: emptyList()
         } else {
