2c521a854fcf345248ca8e68906a7b4f4dcf1c63
hossainsafwan <safwanhossain@live.com>
2024-02-16 16:00:08 -0500

    put @Binds instead of @Provides for impl of repository interface

--- CleanArchMVVM/app/src/main/java/com/example/cleanarchitecture/data/repositories/di/RepositoryModule.kt
+++ CleanArchMVVM/app/src/main/java/com/example/cleanarchitecture/data/repositories/di/RepositoryModule.kt
@@ -3,13 +3,14 @@ package com.example.cleanarchitecture.data.repositories.di
 import com.example.cleanarchitecture.data.network.CountryAPI
 import com.example.cleanarchitecture.data.repositories.AllCountriesRepository
 import com.example.cleanarchitecture.data.repositories.AllCountriesRepositoryImpl
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 
 @Module
-class RepositoryModule {
+abstract class RepositoryModule {
 
-    @Provides
-    fun provideAllCountryRepository(countryAPI: CountryAPI): AllCountriesRepository =
-        AllCountriesRepositoryImpl(countryAPI)
+    @Binds
+    abstract fun provideAllCountryRepository(allCountriesRepositoryImpl: AllCountriesRepositoryImpl)
+            : AllCountriesRepository
 }
