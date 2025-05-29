8a221b55a85b388d0170754b9ef062a6d6b6fb63
hossainsafwan <safwanhossain@live.com>
2025-05-29 15:20:09 -0400

    Clean up

--- CleanArchMVVM/app/src/main/java/com/example/cleanarchitecture/ui/compose/StatefulCountryListScreen.kt
+++ CleanArchMVVM/app/src/main/java/com/example/cleanarchitecture/ui/compose/StatefulCountryListScreen.kt
@@ -1,25 +1,12 @@
 package com.example.cleanarchitecture.ui.compose
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.example.cleanarchitecture.R
 import com.example.cleanarchitecture.ui.models.CountryListUIState
 import com.example.cleanarchitecture.ui.models.CountryUIModel
 import com.example.cleanarchitecture.ui.viewmodels.CountryViewModel
@@ -59,45 +46,6 @@ fun StatefulCountryListScreen(viewModel: CountryViewModel) {
         },
         onRefresh = { viewModel.getCountries() },
     )
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun StatelessCountryListScreen(
-    countryList: List<CountryUIModel>,
-    searchValue: String,
-    isLoading: Boolean,
-    onValueChange: (String) -> Unit,
-    onRefresh: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    Column(
-        modifier = modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-    ) {
-        TextField(
-            value = searchValue,
-            label = {
-                Text(text = stringResource(R.string.search_hint))
-            },
-            onValueChange = { query ->
-                onValueChange(query)
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentHeight(),
-            maxLines = 1
-        )
-
-        PullToRefreshBox(
-            isRefreshing = isLoading,
-            onRefresh = onRefresh,
-            modifier = Modifier
-        ) {
-            CountryList(countryList = countryList, modifier = Modifier.padding(top = 8.dp))
-        }
-    }
 }
 
 @Preview(showBackground = true)
---
+++ CleanArchMVVM/app/src/main/java/com/example/cleanarchitecture/ui/compose/StatelessCountryListScreen.kt
@@ -0,0 +1,56 @@
+package com.example.cleanarchitecture.ui.compose
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.cleanarchitecture.R
+import com.example.cleanarchitecture.ui.models.CountryUIModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun StatelessCountryListScreen(
+    countryList: List<CountryUIModel>,
+    searchValue: String,
+    isLoading: Boolean,
+    onValueChange: (String) -> Unit,
+    onRefresh: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        TextField(
+            value = searchValue,
+            label = {
+                Text(text = stringResource(R.string.search_hint))
+            },
+            onValueChange = { query ->
+                onValueChange(query)
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            maxLines = 1
+        )
+
+        PullToRefreshBox(
+            isRefreshing = isLoading,
+            onRefresh = onRefresh,
+            modifier = Modifier
+        ) {
+            CountryList(countryList = countryList, modifier = Modifier.padding(top = 8.dp))
+        }
+    }
+}
