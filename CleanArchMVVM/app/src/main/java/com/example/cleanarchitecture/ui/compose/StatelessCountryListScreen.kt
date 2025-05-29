d64b9d02356a3458dbdcd235a8c4e3a55cff0508
hossainsafwan <safwanhossain@live.com>
2025-05-29 17:08:43 -0400

    remove click indication

--- CleanArchMVVM/app/src/main/java/com/example/cleanarchitecture/ui/compose/StatelessCountryListScreen.kt
+++ CleanArchMVVM/app/src/main/java/com/example/cleanarchitecture/ui/compose/StatelessCountryListScreen.kt
@@ -1,5 +1,7 @@
 package com.example.cleanarchitecture.ui.compose
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,8 +12,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
 import com.example.cleanarchitecture.R
 import com.example.cleanarchitecture.ui.models.CountryUIModel
@@ -26,10 +31,16 @@ fun StatelessCountryListScreen(
     onRefresh: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+
     Column(
         modifier = modifier
             .fillMaxWidth()
             .fillMaxHeight()
+            .clickable(
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() }
+            ) { keyboardController?.hide() }
     ) {
         TextField(
             value = searchValue,
